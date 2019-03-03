420cd45626bb57a66c21ec3ec6ee34397c8039bc
poyaz <pooya_azarpour@yahoo.com>
2019-03-03 12:28:52 +0330

    add home, end bindkey

--- .zsh/bindkeys.zsh
+++ .zsh/bindkeys.zsh
@@ -4,8 +4,16 @@
 
 bindkey "^K"      kill-whole-line                      # ctrl-k
 bindkey "^R"      history-incremental-search-backward  # ctrl-r
-bindkey "^A"      beginning-of-line                    # ctrl-a  
+bindkey "^A"      beginning-of-line                    # ctrl-a 
+bindkey "\eOH"    beginning-of-line                    # home
+bindkey "\e[H"    beginning-of-line                    # home
+bindkey "\e[1~"   beginning-of-line                    # home
+bindkey "\e[7~"   beginning-of-line                    # home
 bindkey "^E"      end-of-line                          # ctrl-e
+bindkey "\eOF"    end-of-line                          # end
+bindkey "\e[F"    end-of-line                          # end
+bindkey "\e[4~"   end-of-line                          # end
+bindkey "\e[8~"   end-of-line                          # end
 bindkey "[B"      history-search-forward               # down arrow
 bindkey "[A"      history-search-backward              # up arrow
 bindkey "^D"      delete-char                          # ctrl-d
